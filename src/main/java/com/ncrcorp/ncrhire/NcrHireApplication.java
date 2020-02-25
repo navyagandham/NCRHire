6b6807cfb20d84fb7b90ef6c30ce00b4e1790287
Rajesh Bammidi <rajesh.bammidy@gmail.com>
2020-02-25 15:11:39 +0530

    Initial commit

---
+++ src/main/java/com/ncrcorp/ncrhire/NcrHireApplication.java
@@ -0,0 +1,13 @@
+package com.ncrcorp.ncrhire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcrHireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NcrHireApplication.class, args);
+    }
+
+}
